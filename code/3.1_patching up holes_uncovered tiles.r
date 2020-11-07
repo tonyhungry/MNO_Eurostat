2a2fb7c6233f601a4297612c4fb6b30387bd2033
Marco Ramljak <45019651+R-ramljak@users.noreply.github.com>
2020-11-07 12:19:56 +0100

    implementing spatial clustering to patch up coverage holes

--- code/3.1_patching up holes_uncovered tiles.r
+++ code/3.1_patching up holes_uncovered tiles.r
@@ -3,16 +3,24 @@
 
 library(sf)
 library(tidyverse)
+library(igraph)
 
 census.de.100m.tile <- readRDS("C:/Users/Marco/OneDrive - Universiteit Utrecht/MNO/working objects/census.tile.final.rds") %>% 
   dplyr::select(-c(cluster_id, pop.raster, cluster.tile.n))
 
-dev.to.cell.classified <- readRDS("C:/Users/Marco/OneDrive - Universiteit Utrecht/MNO/working objects/dev.to.cell.classified.final.rds")
+dev.to.cell.classified <- readRDS("C:/Users/Marco/OneDrive - Universiteit Utrecht/MNO/working objects/dev.to.cell.classified.rds")
 
 coverage.areas <- readRDS("C:/Users/Marco/OneDrive - Universiteit Utrecht/MNO/working objects/coverage.areas.rds")
 
 
 # aggregating and specifing the tiles that are uncovered 
+tiles.cat.old <- dev.to.cell.classified.old %>% 
+  filter(!weight.pij == 0) %>% 
+  dplyr::select(internal.id, coverage.kind) %>% 
+  st_drop_geometry() %>% 
+  group_by(internal.id) %>% 
+  summarise(count = n())
+
 tiles.cat <- dev.to.cell.classified %>% 
   filter(!weight.pij == 0) %>% 
   dplyr::select(internal.id, coverage.kind) %>% 
@@ -21,7 +29,16 @@ tiles.cat <- dev.to.cell.classified %>%
   summarise(count = n())
 
 # the uncovered tiles resemble three clusters
-missings <- anti_join(census.de.100m.tile, tiles.cat, by = "internal.id") %>% 
+missings.old <- anti_join(census.de.100m.tile, tiles.cat.old, by = "internal.id") 
+missings <- anti_join(census.de.100m.tile, tiles.cat, by = "internal.id") 
+
+missings.buffer <- st_buffer(missings, 100)
+missings.int <- st_intersects(missings.buffer, missings)
+missings.graph <- graph.adjlist(missings.int)
+missings.clust <- components(missings.graph)
+table(missings.clust$membership)
+
+%>% 
   mutate(group.tower = case_when(str_detect(internal.id, "^5|^6") ~ "1",
                                  str_detect(internal.id, "^12") ~ "2",
                                  str_detect(internal.id, "^17") ~ "3"))
@@ -30,10 +47,14 @@ missings <- anti_join(census.de.100m.tile, tiles.cat, by = "internal.id") %>%
 # detect missing clusters
 missings %>% 
   ggplot() +
-  geom_sf(aes(col = group.tower))
+  geom_sf(data = missings.buffer, fill = "green") +
+  geom_sf()
+
+aes(col = group.tower))
 
 # spatially aggregate the clusters and develop the adjusted tower position
 coordinates <- missings %>% 
+  mutate(group.tower = missings.clust$membership) %>% 
   group_by(group.tower) %>% 
   summarize(geometry = st_union(geometry)) %>% 
   mutate(tower.position = st_centroid(geometry) - 1800) %>% # adjusting the deviance of the tower centroid to get the tiles close to the antennas centroid in ordr to pass the threshold
