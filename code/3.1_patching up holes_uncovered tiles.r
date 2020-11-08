b72b58ef2bd07645e3a2d7c01b7fac3f75524c05
Marco Ramljak <45019651+R-ramljak@users.noreply.github.com>
2020-11-08 12:57:00 +0100

    adjusting after laptop change

--- code/3.1_patching up holes_uncovered tiles.r
+++ code/3.1_patching up holes_uncovered tiles.r
@@ -14,13 +14,6 @@ coverage.areas <- readRDS("C:/Users/Marco/OneDrive - Universiteit Utrecht/MNO/wo
 
 
 # aggregating and specifing the tiles that are uncovered 
-tiles.cat.old <- dev.to.cell.classified.old %>% 
-  filter(!weight.pij == 0) %>% 
-  dplyr::select(internal.id, coverage.kind) %>% 
-  st_drop_geometry() %>% 
-  group_by(internal.id) %>% 
-  summarise(count = n())
-
 tiles.cat <- dev.to.cell.classified %>% 
   filter(!weight.pij == 0) %>% 
   dplyr::select(internal.id, coverage.kind) %>% 
@@ -28,38 +21,35 @@ tiles.cat <- dev.to.cell.classified %>%
   group_by(internal.id) %>% 
   summarise(count = n())
 
-# the uncovered tiles resemble three clusters
-missings.old <- anti_join(census.de.100m.tile, tiles.cat.old, by = "internal.id") 
+# how many tiles are not sufficiently covered
 missings <- anti_join(census.de.100m.tile, tiles.cat, by = "internal.id") 
 
-missings.buffer <- st_buffer(missings, 100)
+# workflow: spanning a buffer of 700m over each tile and finding groups that intersect with each other --> clusters
+missings.buffer <- st_buffer(missings, 700)
 missings.int <- st_intersects(missings.buffer, missings)
 missings.graph <- graph.adjlist(missings.int)
 missings.clust <- components(missings.graph)
 table(missings.clust$membership)
 
-%>% 
-  mutate(group.tower = case_when(str_detect(internal.id, "^5|^6") ~ "1",
-                                 str_detect(internal.id, "^12") ~ "2",
-                                 str_detect(internal.id, "^17") ~ "3"))
+# summariszing their geometries and their tile size of each cluster
+missings.clustered <- missings %>% 
+  mutate(group.tower = missings.clust$membership) %>% 
+  group_by(group.tower) %>% 
+  summarize(geometry = st_union(geometry), count.tiles = n())
 
+# manually placing towers at the centroid of each cluster and then moving them partially down to place coverage areas over them missing tiles
+coordinates <- missings.clustered %>% 
+  mutate(tower.kind = case_when(count.tiles >= 500 ~ "Rural",
+                                TRUE ~ "Suburban")) %>% # speccifiyng which kind of tower needs to be placed
+  mutate(tower.position = st_centroid(geometry) - c(1000, 1800)) %>% # adjusting the deviance of the tower centroid to get the tiles close to the antennas centroid in ordr to pass the threshold
+  st_drop_geometry() %>% 
+  st_sf(sf_column_name = "tower.position", crs = 3035) 
 
-# detect missing clusters
+# visual analysis: detect missing clusters
 missings %>% 
   ggplot() +
   geom_sf(data = missings.buffer, fill = "green") +
   geom_sf()
-
-aes(col = group.tower))
-
-# spatially aggregate the clusters and develop the adjusted tower position
-coordinates <- missings %>% 
-  mutate(group.tower = missings.clust$membership) %>% 
-  group_by(group.tower) %>% 
-  summarize(geometry = st_union(geometry)) %>% 
-  mutate(tower.position = st_centroid(geometry) - 1800) %>% # adjusting the deviance of the tower centroid to get the tiles close to the antennas centroid in ordr to pass the threshold
-  st_drop_geometry() %>% 
-  st_sf(sf_column_name = "tower.position", crs = 3035) 
 
 # Visual analysis if tower positions apply correct
 coverage.areas %>% 
@@ -70,12 +60,23 @@ coverage.areas %>%
   geom_sf(data = coordinates)
 
 # Save the adjusted tower position coordinates to bind_rows in 2_Radio cell generation
-coordinates.final <- coordinates %>% 
+coordinates.rural.final <- coordinates %>% 
+  # filter(tower.kind == "Rural") %>% 
   st_coordinates() %>% 
   as_tibble()
 
+# coordinates.suburban.final <- coordinates %>% 
+#   filter(tower.kind == "Suburban") %>% 
+#   st_coordinates() %>% 
+#   as_tibble()
 
-saveRDS(coordinates.final, "C:/Users/Marco/OneDrive - Universiteit Utrecht/MNO/working objects/manually placed towers.rds")  
-  
-  
-  
+# coordinates.suburban.final.second <- coordinates %>% 
+#   filter(tower.kind == "Suburban") %>% 
+#   st_coordinates() %>% 
+#   as_tibble()
+
+
+
+saveRDS(coordinates.rural.final, "C:/Users/Marco/OneDrive - Universiteit Utrecht/MNO/working objects/manually placed towers.rural.rds")  
+# saveRDS(coordinates.suburban.final, "C:/Users/ramljak/Desktop/marco/manually placed towers.suburban.rds")  
+# saveRDS(coordinates.suburban.final.second, "C:/Users/ramljak/Desktop/marco/manually placed towers.suburban.second.rds")  
