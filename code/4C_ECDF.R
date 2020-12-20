c236e97565d60ada33567e1fc9904dbac0e9d326
Marco Ramljak <45019651+R-ramljak@users.noreply.github.com>
2020-12-20 15:59:46 +0100

    implemementing ECCDF and ECDF plots

--- code/4C_ECDF.R
+++ code/4C_ECDF.R
@@ -1,135 +1,207 @@
-require(tidyverse)
-require(ggthemes)
+library(tidyverse)
+library(ggthemes)
+library(sf)
+
+setwd("C:/Users/Marco/")
 
 # Loading In
-voro <- readRDS("~/OneDrive - Vysoká škola ekonomická v Praze/YAY/Estimates/Voronoi both/Voronoi.estimates.rds")
-equal <- readRDS("~/OneDrive - Vysoká škola ekonomická v Praze/YAY/Estimates/P.equal/u.est.non.inf.P.equal.part801_1000_new.rds")[,202]
-true <- readRDS("~/OneDrive - Vysoká škola ekonomická v Praze/YAY/Estimates/P.oracle/u.est.non.inf.P.oracle.part801_1000_new.rds")[,202]
+voro <- readRDS("Vysoká škola ekonomická v Praze/Tony Wei Tse Hung - YAY/Estimates/Voronoi both/Voronoi.estimates.rds")
+equal <- readRDS("Vysoká škola ekonomická v Praze/Tony Wei Tse Hung - YAY/Estimates/P.equal/u.est.non.inf.P.equal.part801_1000_new.rds")[,c(1,202)]
+# oracle <- readRDS("Vysoká škola ekonomická v Praze/Tony Wei Tse Hung - YAY/Estimates/P.oracle/u.est.non.inf.P.oracle.part801_1000_new.rds")[,202]
+census.de.100m.tile <- readRDS("Vysoká škola ekonomická v Praze/Tony Wei Tse Hung - YAY/working objects/census.tile.final.rds") 
 
-# Voronoi Antenna
-voro.ant.ECCDF = voro %>% 
-  # sample_n(1000) %>%
-  mutate(pop.plot = voronoi.est.antenna + 1) %>%  
-  arrange(pop.plot) %>%  
-  mutate(prob = 1 / n()) %>%  
-  mutate(cum.prob = cumsum(prob)) %>%  
-  mutate(log10.cum.prob.comp = 1 - cum.prob) %>%  
-  mutate(log10.pop = pop.plot) %>%  
-  mutate(cum.prob.comp = 1 - cum.prob)
 
-voro.ant.eccdf.plot = voro.ant.ECCDF %>%   
+## This function computes ECCDF dataframes with log10 transformations of te x and y axis
+
+custom_ecdf_prep <- function(df) {
+  data <- df %>% 
+    mutate(pop.plot = pop + 1) %>%  
+    arrange(pop.plot) %>%  
+    mutate(prob = 1 / n()) %>%  
+    mutate(cum.prob = cumsum(prob)) %>%  
+    mutate(cum.prob.comp = 1 - cum.prob) %>%  
+    mutate(log10.cum.prob.comp = log10(cum.prob.comp)) %>% 
+    mutate(log10.pop = log10(pop.plot)) %>%  
+    mutate(cum.prob.comp = 1 - cum.prob)
+  
+  return(data)
+}
+
+## example
+
+## Voronoi Antenna
+# voro.ant.ECCDF <- voro %>%
+#   ungroup() %>%
+#   dplyr::select(internal.id, voronoi.est.antenna) %>%
+#   mutate(pop.plot = voronoi.est.antenna + 1) %>%
+#   arrange(pop.plot) %>%
+#   mutate(prob = 1 / n()) %>%
+#   mutate(cum.prob = cumsum(prob)) %>%
+#   mutate(log10.cum.prob.comp = 1 - cum.prob) %>%
+#   mutate(log10.pop = log10(pop.plot)) %>%
+#   mutate(cum.prob.comp = 1 - cum.prob) %>%
+#   select(log10.cum.prob.comp, log10.pop) %>%
+#   mutate(log10.cum.prob.comp = round(log10.cum.prob.comp, 4)) %>% # effective plot sample --> faster plotting excluding overplot
+#   distinct() %>%
+#   mutate(estimate = "Voronoi antenna")
+
+###-------------------------------##
+
+# Voronoi antenna
+
+voro.ant.ECCDF <- voro %>% 
+  ungroup() %>% 
+  dplyr::select(internal.id, pop = voronoi.est.antenna) %>% 
+  custom_ecdf_prep() %>% 
+  select(log10.cum.prob.comp, log10.pop) %>%
+  mutate(log10.cum.prob.comp = round(log10.cum.prob.comp, 4)) %>% # effective plot sample --> faster plotting excluding overplot
+  distinct() %>% 
+  mutate(estimate = "Voronoi antenna")
+
+# Voronoi tower
+
+voro.tower.ECCDF <- voro %>% 
+  ungroup() %>% 
+  dplyr::select(internal.id, pop = voronoi.est.tower) %>% 
+  custom_ecdf_prep() %>% 
+  select(log10.cum.prob.comp, log10.pop) %>%
+  mutate(log10.cum.prob.comp = round(log10.cum.prob.comp, 4)) %>% # effective plot sample --> faster plotting excluding overplot
+  distinct() %>% 
+  mutate(estimate = "Voronoi tower")
+
+# Equal prob
+
+MLE.equal.ECCDF <- equal %>% 
+  as_tibble() %>% 
+  ungroup() %>% 
+  dplyr::select(internal.id = j, pop = u200) %>% 
+  custom_ecdf_prep() %>% 
+  select(log10.cum.prob.comp, log10.pop) %>%
+  mutate(log10.cum.prob.comp = round(log10.cum.prob.comp, 4)) %>% # effective plot sample --> faster plotting excluding overplot
+  distinct() %>% 
+  mutate(estimate = "MLE equal P.matrix")
+
+# Oracle prob
+
+# MLE.oracle.ECCDF <- oracle %>% 
+#   as_tibble() %>% 
+#   ungroup() %>% 
+#   dplyr::select(internal.id = j, pop = u200) %>% 
+#   custom_ecdf_prep() %>% 
+#   select(log10.cum.prob.comp, log10.pop) %>%
+#   mutate(log10.cum.prob.comp = round(log10.cum.prob.comp, 4)) %>% # effective plot sample --> faster plotting excluding overplot
+#   distinct() %>% 
+#   mutate(estimate = "MLE oracle P.matrix")
+
+# True dist
+
+true.ECCDF <- census.de.100m.tile %>% 
+  st_drop_geometry() %>% 
+  ungroup() %>% 
+  dplyr::select(internal.id, pop) %>% 
+  custom_ecdf_prep() %>% 
+  select(log10.cum.prob.comp, log10.pop) %>%
+  mutate(log10.cum.prob.comp = round(log10.cum.prob.comp, 4)) %>% # effective plot sample --> faster plotting excluding overplot
+  distinct() %>% 
+  mutate(estimate = "True")
+
+## combining all estimator objects to one for easier plotting
+
+final.ECCDF <- bind_rows(true.ECCDF, voro.ant.ECCDF, voro.tower.ECCDF, MLE.equal.ECCDF, MLE.oracle.ECCDF)
+breaks <- c("True", "Voronoi antenna", "Voronoi tower", "MLE equal P.matrix", "MLE oracle P.matrix")
+
+## ECCDF comparison plot
+
+ECCDF.estimates <- final.ECCDF %>%   
   ggplot() + 
-  geom_point(aes(x = log10.pop, y = log10.cum.prob.comp)) + 
-  scale_color_ptol() + 
-  ggtitle("", subtitle = "ECCDF of Voronoi ~ Antenna") +  
-  labs(y = "Prob(Y > x)", x = "Mobile phones") + 
-  ylim(-10, 0) +
-  theme(legend.position = "bottom") 
+  geom_path(aes(x = log10.pop, y = log10.cum.prob.comp, color = estimate), size = 1) + 
+  scale_color_ptol(breaks = breaks) + 
+  ggtitle("", subtitle = "ECCDF comparison") +  
+  labs(y = "Prob(Y > x)", x = "Mobile phones", color = "Estimator")
 
-voro %>%
-  ggplot(aes(x=voronoi.est.antenna)) + geom_density()
+saveRDS(ECCDF.estimates, "Vysoká škola ekonomická v Praze/Tony Wei Tse Hung - YAY/Estimates/Plot.files/ECCDF.estimates.rds")
 
-voro %>%
-  ggplot(aes(x=voronoi.est.antenna)) + geom_histogram(bins= 5)
 
-voro.ant.ecdf.plot = voro %>%
-  ggplot(aes(voronoi.est.antenna)) + stat_ecdf(pad = F) + 
-  ggtitle("", subtitle = "ECDF of Voronoi ~ Antenna") +
-  labs(y = "Prob(Y > x)", x = "Mobile phones")
+#######-------------------------###
 
-# Voronoi Tower
-voro.tow.ECCDF = voro %>% 
-  # sample_n(1000) %>%
-  mutate(pop.plot = voronoi.est.tower) %>%  
-  arrange(pop.plot) %>%  
-  mutate(prob = 1 / n()) %>%  
-  mutate(cum.prob = cumsum(prob)) %>%  
-  mutate(log10.cum.prob.comp = log10(1 - cum.prob)) %>%  
-  mutate(log10.pop = log10(pop.plot)) %>%  
-  mutate(cum.prob.comp = 1 - cum.prob)
+# ECDF plots
 
-voro.tow.eccdf.plot = voro.tow.ECCDF %>%   
+##############
+
+# Voronoi antenna
+
+voro.ant.ECDF <- voro %>% 
+  ungroup() %>% 
+  dplyr::select(internal.id, pop = voronoi.est.antenna) %>% 
+  custom_ecdf_prep() %>% 
+  select(cum.prob.comp, pop.plot) %>%
+  mutate(cum.prob.comp = round(cum.prob.comp, 4)) %>% # effective plot sample --> faster plotting excluding overplot
+  distinct() %>% 
+  mutate(estimate = "Voronoi antenna")
+
+
+# Voronoi tower
+
+voro.tower.ECDF <- voro %>% 
+  ungroup() %>% 
+  dplyr::select(internal.id, pop = voronoi.est.tower) %>% 
+  custom_ecdf_prep() %>% 
+  select(cum.prob.comp, pop.plot) %>%
+  mutate(cum.prob.comp = round(cum.prob.comp, 4)) %>% # effective plot sample --> faster plotting excluding overplot
+  distinct() %>% 
+  mutate(estimate = "Voronoi tower")
+
+# Equal prob
+
+MLE.equal.ECDF <- equal %>% 
+  as_tibble() %>% 
+  ungroup() %>% 
+  dplyr::select(internal.id = j, pop = u200) %>% 
+  custom_ecdf_prep() %>% 
+  select(cum.prob.comp, pop.plot) %>%
+  mutate(cum.prob.comp = round(cum.prob.comp, 4)) %>% # effective plot sample --> faster plotting excluding overplot
+  distinct() %>% 
+  mutate(estimate = "MLE equal P.matrix")
+
+# Oracle prob
+
+# MLE.oracle.ECDF <- oracle %>%
+#   as_tibble() %>%
+#   ungroup() %>%
+#   dplyr::select(internal.id = j, pop = u200) %>%
+#   custom_ecdf_prep() %>%
+#   select(cum.prob.comp, pop.plot) %>%
+#   mutate(cum.prob.comp = round(cum.prob.comp, 4)) %>% # effective plot sample --> faster plotting excluding overplot
+#   distinct() %>%
+#   mutate(estimate = "MLE oracle P.matrix")
+
+# True dist
+
+true.ECDF <- census.de.100m.tile %>% 
+  st_drop_geometry() %>% 
+  ungroup() %>% 
+  dplyr::select(internal.id, pop) %>% 
+  custom_ecdf_prep() %>% 
+  select(cum.prob.comp, pop.plot) %>%
+  mutate(cum.prob.comp = round(cum.prob.comp, 4)) %>% # effective plot sample --> faster plotting excluding overplot
+  distinct() %>% 
+  mutate(estimate = "True")
+
+## combining all estimator objects to one for easier plotting
+
+final.ECDF <- bind_rows(true.ECDF, voro.ant.ECDF, voro.tower.ECDF, MLE.equal.ECDF, MLE.oracle.ECDF)
+breaks <- c("True", "Voronoi antenna", "Voronoi tower", "MLE equal P.matrix", "MLE oracle P.matrix")
+
+## ECCDF comparison plot
+
+ECDF.estimates <- final.ECDF %>%   
   ggplot() + 
-  geom_point(aes(x = log10.pop, y = log10.cum.prob.comp)) + 
-  scale_color_ptol() + 
-  ggtitle("", subtitle = "ECCDF of Voronoi ~ Tower") +  
-  labs(y = "log10(Prob(Y > x))", x = "log10(Mobile phones)",  
-       colour = "") + 
-  ylim(-10, 0) +
-  theme(legend.position = "bottom") 
+  geom_path(aes(x = pop.plot, y = cum.prob.comp, color = estimate)) + 
+  scale_color_ptol(breaks = breaks, guide = FALSE, expand = c(0, 0)) +
+  labs(y = "", x = "") +
+  theme(plot.title = element_text(size = 10, face = "bold", hjust = 0.5),
+        plot.subtitle = element_text(size = 9, hjust = 0.5),
+        axis.title.x = element_blank(),
+        axis.title.y = element_blank())
 
-voro.ant.ecdf.plot = voro %>%
-  ggplot(aes(voronoi.est.tower)) + stat_ecdf(pad = F) + 
-  ggtitle("", subtitle = "ECDF of Voronoi ~ Tower")
-
-# MLE Equal 
-
-mle.equal.ECCDF = as.data.frame(equal) %>% 
-  # sample_n(1000) %>%
-  mutate(pop.plot = equal + 1) %>%  
-  arrange(pop.plot) %>%  
-  mutate(prob = 1 / n()) %>%  
-  mutate(cum.prob = cumsum(prob)) %>%  
-  mutate(log10.cum.prob.comp = log10(1 - cum.prob)) %>%  
-  mutate(log10.pop = log10(pop.plot)) %>%  
-  mutate(cum.prob.comp = 1 - cum.prob)
-
-mle.equal.eccdf.plot = mle.equal.ECCDF %>%   
-  ggplot() + 
-  geom_point(aes(x = log10.pop, y = log10.cum.prob.comp)) + 
-  scale_color_ptol() + 
-  ggtitle("", subtitle = "ECCDF of MLE ~ Equal") +  
-  labs(y = "log10(Prob(Y > x))", x = "log10(Mobile phones)") + 
-  ylim(-10, 0) +
-  theme(legend.position = "bottom") 
-
-mle.equal.ecdf.plot = as.data.frame(equal) %>%
-  ggplot(aes(equal)) + stat_ecdf(pad = F) + 
-  ggtitle("", subtitle = "ECDF of MLE ~ Equal")
-
-# MLE true 
-
-mle.true.ECCDF = as.data.frame(true) %>% 
-  # sample_n(1000) %>%
-  mutate(pop.plot = true + 1) %>%  
-  arrange(pop.plot) %>%  
-  mutate(prob = 1 / n()) %>%  
-  mutate(cum.prob = cumsum(prob)) %>%  
-  mutate(log10.cum.prob.comp = log10(1 - cum.prob)) %>%  
-  mutate(log10.pop = log10(pop.plot)) %>%  
-  mutate(cum.prob.comp = 1 - cum.prob)
-
-mle.true.eccdf.plot = mle.true.ECCDF %>%   
-  ggplot() + 
-  geom_point(aes(x = log10.pop, y = log10.cum.prob.comp)) + 
-  scale_color_ptol() + 
-  ggtitle("", subtitle = "ECCDF of MLE ~ True") +  
-  labs(y = "log10(Prob(Y > x))", x = "log10(Mobile phones)") + 
-  ylim(-10, 0) +
-  theme(legend.position = "bottom") 
-
-mle.true.ecdf.plot = as.data.frame(true) %>%
-  ggplot(aes(true)) + stat_ecdf(pad = F) + 
-  ggtitle("", subtitle = "ECDF of MLE ~ True")
-
-#### Potential New Work Flow for ECCDF ####
-voroa = cbind(voro, equal, true)
-colnames(voroa) = c("internal.id","pop","u.antenna","u.tower","u.equal","u.true")
-
-eccdf <- function(x) { return( 1 - ecdf(x)(x) ) } # ECCDF function
-
-vorol = voroa %>%
-  pivot_longer(-c(pop,internal.id), names_to = "type", values_to = "final") %>%
-  mutate(e = eccdf(final))
-
-# ECDF
-vorol %>%
-  ggplot(aes(y =final)) + stat_ecdf() + 
-  facet_grid(vars(type),scales="free") + 
-  scale_color_ptol(breaks = c("u.antenna","u.tower","u.equal","u.true"))
-
-# ECCDF
-vorol %>%
-  ggplot(aes(y = e)) + geom_line() + 
-  facet_grid(vars(type),scales="free") + 
-  scale_color_ptol(breaks = c("u.antenna","u.tower","u.equal","u.true"))
+saveRDS(ECDF.estimates, "Vysoká škola ekonomická v Praze/Tony Wei Tse Hung - YAY/Estimates/Plot.files/ECDF.estimates.rds")
