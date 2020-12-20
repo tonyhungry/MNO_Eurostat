ffb41e047778070de7b38dce5bd13a8847ab2503
Marco Ramljak <45019651+R-ramljak@users.noreply.github.com>
2020-12-20 23:52:21 +0100

    implemented proportion plot

--- code/4C_ECDF_new.R
+++ code/4C_ECDF_new.R
@@ -205,3 +205,80 @@ ECDF.estimates <- final.ECDF %>%
         axis.title.y = element_blank())
 
 saveRDS(ECDF.estimates, "~/OneDrive - Vysoká škola ekonomická v Praze/YAY/Estimates/Plot.files/ECDF.estimates.rds")
+
+
+#### Only true plot
+
+# True dist
+
+true.ECCDF.first <- census.de.100m.tile %>% 
+  st_drop_geometry() %>% 
+  ungroup() %>% 
+  custom_ecdf_prep() %>% 
+  dplyr::select(log10.cum.prob.comp, log10.pop, pop.area.kind) %>%
+  mutate(log10.cum.prob.comp = round(log10.cum.prob.comp, 4)) %>% # effective plot sample --> faster plotting excluding overplot
+  distinct()
+
+
+true.ECDF.first <- census.de.100m.tile %>% 
+  st_drop_geometry() %>% 
+  ungroup() %>% 
+  custom_ecdf_prep() %>% 
+  dplyr::select(cum.prob.comp, pop.plot, pop.area.kind) %>%
+  mutate(cum.prob.comp = round(cum.prob.comp, 4)) %>% # effective plot sample --> faster plotting excluding overplot
+  distinct()
+
+breaks.first <- c("Uninhabitated", "Rural", "Suburban", "Urban")
+ECCDF.true <- true.ECCDF.first %>%   
+  ggplot() + 
+  geom_point(aes(x = log10.pop, y = log10.cum.prob.comp, color = pop.area.kind)) + 
+  scale_color_ptol(breaks = breaks.first) + 
+  ggtitle("", subtitle = "ECCDF true mobile phone density") +  
+  labs(y = "Prob(Y > x)", x = "Mobile phones", color = "")
+
+saveRDS(ECCDF.true, "Vysoká škola ekonomická v Praze/Tony Wei Tse Hung - YAY/Estimates/Plot.files/ECCDF.true.rds")
+
+
+ECDF.true <- true.ECDF.first %>%   
+  ggplot() + 
+  geom_point(aes(x = pop.plot, y = cum.prob.comp, color = pop.area.kind)) + 
+  scale_color_ptol(breaks = breaks.first, guide = FALSE, expand = c(0, 0)) +
+  labs(y = "", x = "") +
+  theme(plot.title = element_text(size = 10, face = "bold", hjust = 0.5),
+        plot.subtitle = element_text(size = 9, hjust = 0.5),
+        axis.title.x = element_blank(),
+        axis.title.y = element_blank())
+
+saveRDS(ECDF.true, "Vysoká škola ekonomická v Praze/Tony Wei Tse Hung - YAY/Estimates/Plot.files/ECDF.true.rds")
+
+
+
+#### bar plot pop area kind
+
+true.ECCDF.complete <- census.de.100m.tile %>% 
+  st_drop_geometry() %>% 
+  ungroup() %>% 
+  custom_ecdf_prep() %>% 
+  dplyr::select(log10.cum.prob.comp, log10.pop, pop.area.kind) %>%
+  mutate(log10.cum.prob.comp = round(log10.cum.prob.comp, 4)) 
+
+prop.class <- data.frame( 
+  class = c("Rural","Suburban","Urban"), 
+  n = summary(as.factor(true.ECCDF.complete$pop.area.kind)), 
+  prop = round(summary(as.factor(true.ECCDF.complete$pop.area.kind)) / length(census.de.100m.tile$internal.id), digits = 4) * 100)  %>% 
+  mutate(class = factor(class)) %>% 
+  arrange(desc(class)) %>% 
+  mutate(lab.ypos = cumsum(prop) - 0.5 * prop) 
+
+tile.prop.plot <- prop.class %>% 
+  ggplot(aes(x = "", y = prop, fill = class)) + 
+  geom_bar(width = 0.5, stat = "identity", color = "white") + 
+  scale_fill_ptol(breaks = c("Rural", "Suburban", "Urban")) +
+  geom_text(aes(y = lab.ypos, label = paste0(prop, "%")), color = "black") +
+  labs(x = "", y = "Proportion of tiles", title = "", fill = "",
+       subtitle = "Fig. 2a: Tile proportion") + 
+  theme(plot.title = element_text(size = 10, face = "bold", hjust = 0.5),
+        plot.subtitle = element_text(size = 9, hjust = 0.5))
+
+
+saveRDS(tile.prop.plot, "Vysoká škola ekonomická v Praze/Tony Wei Tse Hung - YAY/Estimates/Plot.files/tile.prop.plot.rds")
